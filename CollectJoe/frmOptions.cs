3201c2edb24c691bff724af27eeba144d56477b6
vmadmin <vmadmin@BMWP2>
2018-05-28 16:08:00 +0200

    Methoden für frmOptions

--- CollectJoe/frmOptions.cs
+++ CollectJoe/frmOptions.cs
@@ -16,5 +16,67 @@ namespace CollectJoe
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// gibt die Horizontale länge zurück
+        /// </summary>
+        /// <returns>int länge</returns>
+        public int GetHorizontal()
+        {
+            int x = 20;
+            try
+            {
+                x = Convert.ToInt16(txtBoxenHorizontal.Text);
+                if (x == null || x == 0 || x > 20)
+                {
+                    x = 20;
+                    return x;
+                } else if(x < 1)
+                {
+                    x = 10;
+                    return x;
+                }
+            } catch(Exception e)
+            {
+                Console.WriteLine("Keine Zahl eingegeben!");
+            }
+            return x;
+        }
+
+        /// <summary>
+        /// gibt die Vertikale länge zurück
+        /// </summary>
+        /// <returns>int höhe</returns>
+        public int GetVertical()
+        {
+            int x = 10;
+            try
+            {
+                x = Convert.ToInt16(txtBoxenVertikal.Text);
+                if (x == null || x == 0 || x > 10)
+                {
+                    x = 10;
+                    return x;
+                }
+                else if (x < 1)
+                {
+                    x = 10;
+                    return x;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Keine Zahl eingegeben!");
+            }
+            return x;
+        }
+
+        public int GetValue(string txtName)
+        {
+            Control[] controls = this.Controls.Find(txtName, true);
+            TextBox activeTextBox = (TextBox)controls[0];
+
+            return 0;
+        }
     }
 }
